5a89c921063d6c948b6d8fe94063ac4f78f7f5f1
Sam Fishback <smf15shback@gmail.com>
2024-06-19 02:27:22 -0700

    added functional secondHand

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ function adjustFontSize(){
   document.documentElement.style.setProperty('--device-size', `${deviceSize}px`);
 }
 
-window.onload = adjustFontSize;
+function updateSecondHand(){
+  let secondHand = document.getElementById('secondHand');
+  let now = new Date();
+  let seconds = now.getSeconds();
+  let secondsDegrees = seconds * 6; 
+//each second is 6deg. offset of 90deg is set in css
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
 
+window.onload = adjustFontSize;
 window.addEventListener('resize', adjustFontSize);
+setInterval(updateSecondHand, 1000);
+updateSecondHand();
