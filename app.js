87813ab7e74027ec18f24cc3807e3c3108b70291
Sam Fishback <smf15shback@gmail.com>
2024-06-22 02:45:10 -0700

    added functions to hours and minutes

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ function adjustFontSize(){
   document.documentElement.style.setProperty('--device-size', `${deviceSize}px`);
 }
 
-function updateSecondHand(){
+function updateHands(){
   let secondHand = document.getElementById('secondHand');
+  let minuteHand = document.getElementById('minuteHand');
+  let hourHand = document.getElementById('hourHand');
   let now = new Date();
   let seconds = now.getSeconds();
+  let minutes = now.getMinutes();
+// translating a 24hr time to a 12hr clock
+  let hours = now.getHours() % 12;
+// each second is 6deg. offset of 90deg is set in css
   let secondsDegrees = seconds * 6; 
-//each second is 6deg. offset of 90deg is set in css
+  let minutesDegrees = minutes * 6;
+// will need to adjust hourHand by 6deg every 10min
+// every hour is 30deg
+  let hoursDegrees = hours * 30;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 window.onload = adjustFontSize;
 window.addEventListener('resize', adjustFontSize);
-setInterval(updateSecondHand, 1000);
-updateSecondHand();
+setInterval(updateHands, 1000);
+updateHands();
