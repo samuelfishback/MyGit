b554db169fe5cd8bce0b95773fc99f8180c313ed
Sam Fishback <smf15shback@gmail.com>
2024-06-23 01:05:36 -0700

    removed unnecessary comments

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 function adjustFontSize(){
-  /* old version
-  let clockWidth = document.getElementById('innerCircle').offsetWidth;
-  document.documentElement.style.setProperty('--clock-width', `${clockWidth}px`);
-  */
   let deviceSize = (window.innerWidth + window.innerHeight) / 2;
   document.documentElement.style.setProperty('--device-size', `${deviceSize}px`);
 }
