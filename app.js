6061b636560e00648c723a453b821793416afbf3
Sam Fishback <smf15shback@gmail.com>
2024-07-02 16:51:28 -0700

    functional digital times & addedoutline to hands & changed digital outer container to padding around content & added padding to analog clock number 6

--- app.js
+++ app.js
@@ -3,27 +3,47 @@ function adjustFontSize(){
   document.documentElement.style.setProperty('--device-size', `${deviceSize}px`);
 }
 
+let globalTimes = {};
+
+function grabGlobalTime(){
+  let now = new Date();
+  globalTimes.seconds = now.getSeconds();
+  globalTimes.minutes = now.getMinutes();
+  globalTimes.hours = now.getHours();
+}
+
 function updateHands(){
   let secondHand = document.getElementById('secondHand');
   let minuteHand = document.getElementById('minuteHand');
   let hourHand = document.getElementById('hourHand');
-  let now = new Date();
-  let seconds = now.getSeconds();
-  let minutes = now.getMinutes();
-// translating a 24hr time to a 12hr clock
-  let hours = now.getHours() % 12;
 // each second is 6deg. offset of 90deg is set in css
-  let secondsDegrees = seconds * 6; 
-  let minutesDegrees = minutes * 6;
+  let secondsDegrees = globalTimes.seconds * 6; 
+  let minutesDegrees = globalTimes.minutes * 6;
 // hourHand moves by 6deg every 10min
 // every hour is 30deg
-  let hoursDegrees = (hours * 30) + (Math.floor(minutes / 10) * 6);
+// translate a 24hr time to a 12hr clock
+  let hoursDegrees = (globalTimes.hours % 12 * 30) + (Math.floor(globalTimes.minutes / 10) * 6);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
+function updateDigital(){
+  let digitalSecond = document.getElementById('digitalSecond');
+  let digitalMinute = document.getElementById('digitalMinute');
+  let digitalHour = document.getElementById('digitalHour');
+  digitalSecond.textContent = globalTimes.seconds < 10 ? '0' + globalTimes.seconds : globalTimes.seconds;
+  digitalMinute.textContent = globalTimes.minutes < 10 ? '0' + globalTimes.minutes : globalTimes.minutes;
+  digitalHour.textContent = globalTimes.hours;
+}
+
 window.onload = adjustFontSize;
 window.addEventListener('resize', adjustFontSize);
-setInterval(updateHands, 1000);
+setInterval(() => {
+  grabGlobalTime();
+  updateHands();
+  updateDigital();
+  }, 1000);
+grabGlobalTime();
 updateHands();
+updateDigital();
